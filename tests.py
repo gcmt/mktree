feb9f10b50709f1d11b2ec748d85b78755ea3d1e
Giacomo Comitti <dev@gcomit.com>
2019-09-06 06:25:21 +0200

    Refactor tests

--- tests.py
+++ tests.py
@@ -9,25 +9,16 @@ mktree = importlib.machinery.SourceFileLoader("mktree", module_path).load_module
 class TestDirTree(unittest.TestCase):
     def test_tree_parsing(self):
 
-        t = mktree.DirTree.load("a/b")
-        expected = ["a/b"]
-        self.assertEqual(set(t), set(expected))
+        load = mktree.DirTree.load
 
-        t = mktree.DirTree.load("a/b,c")
-        expected = ["a/b", "a/c"]
-        self.assertEqual(set(t), set(expected))
+        def eq(a, b):
+            self.assertEqual(set(a), set(b))
 
-        t = mktree.DirTree.load("a/b,c.d")
-        expected = ["a/b", "a/c", "d"]
-        self.assertEqual(set(t), set(expected))
-
-        t = mktree.DirTree.load("a/b/c,d..e/f")
-        expected = ["a/b/c", "a/b/d", "e/f"]
-        self.assertEqual(set(t), set(expected))
-
-        t = mktree.DirTree.load("a/b/c,d..e/f.g.h")
-        expected = ["a/b/c", "a/b/d", "e/f", "g", "h"]
-        self.assertEqual(set(t), set(expected))
+        eq(load("a/b"), ["a/b"])
+        eq(load("a/b,c"), ["a/b", "a/c"])
+        eq(load("a/b,c.d"), ["a/b", "a/c", "d"])
+        eq(load("a/b/c,d..e/f"), ["a/b/c", "a/b/d", "e/f"])
+        eq(load("a/b/c,d..e/f.g.h"), ["a/b/c", "a/b/d", "e/f", "g", "h"])
 
 
 if __name__ == "__main__":
